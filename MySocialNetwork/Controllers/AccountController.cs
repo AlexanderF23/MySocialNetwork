4f2a5054a12bca1cdc120dbbb9c50dfa50b73b3f
Alexander <Alexander.Friedrichsen7@gmail.com>
2025-07-18 15:39:57 +0200

    Initial commit — created MySocialNetwork with simple login and registration functionality

---
+++ MySocialNetwork/Controllers/AccountController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MySocialNetwork.Services;
+
+
+namespace MySocialNetwork.Controllers;
+
+public class AccountController : Controller
+{
+    public IActionResult Login() => View();
+
+    [HttpPost]
+    public IActionResult Login(string username, string password)
+    {
+        if (UserService.ValidateLogin(username, password))
+        {
+            HttpContext.Session.SetString("User", username);
+            return RedirectToAction("Index", "Home");
+        }
+
+        ViewBag.Error = "Forkert Brugernavn eller adgangskode";
+        return View();
+    }
+
+
+    public IActionResult Register() => View();
+
+    [HttpPost]
+    public IActionResult Register(string username, string password)
+    {
+        if (UserService.Register(username, password))
+            return RedirectToAction("Login");
+
+        ViewBag.Error = "Brugernavn eksisterer allerede";
+        return View();
+    }
+
+    public IActionResult Logout()
+    {
+        HttpContext.Session.Remove("User");
+        return RedirectToAction("Login");
+    }
+
+}
---
+++ MySocialNetwork/Services/UserService.cs
@@ -0,0 +1,32 @@
+﻿using MySocialNetwork.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace MySocialNetwork.Services;
+
+    public class UserService
+    {
+        private static List<User> Users = new();
+
+        public static bool Register(string username, string password)
+        {
+            if (Users.Any(u => u.Username == username)) return false;
+
+            Users.add(new User
+            {
+                Username = username,
+                Password = password
+            });
+
+            return true;
+        }
+
+        public static bool ValidateLogin(string username, string password)
+        {
+            return Users.Any(u => u.Username == username && u.Password == password);
+        }
+    
+
+
+    }
