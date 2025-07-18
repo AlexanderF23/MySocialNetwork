5fc9faa3c45826fb30dadaa0aa6a413c3802f867
Alexander <Alexander.Friedrichsen7@gmail.com>
2025-07-18 18:31:25 +0200

    Added username and password Validation.

--- MySocialNetwork/Controllers/AccountController.cs
+++ MySocialNetwork/Controllers/AccountController.cs
@@ -1,6 +1,7 @@
 ﻿
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Http;
+using MySocialNetwork.Models;
 
 namespace MySocialNetwork.Controllers
 {
@@ -9,9 +10,25 @@ namespace MySocialNetwork.Controllers
         private static Dictionary<string, string> users = new Dictionary<string, string>();
 
         [HttpGet]
-        public IActionResult Login()
+        public IActionResult Login(UserViewModel model)
         {
-            return View();
+            if (!ModelState.IsValid)
+            return View(model);
+
+            if (string.IsNullOrWhiteSpace(model.Username) || string.IsNullOrWhiteSpace(model.Password))
+            {
+                ModelState.AddModelError("", "Udfyld både brugernavn og adgangskode.");
+                return View(model);
+            }
+
+            if (users.TryGetValue(model.Username, out var correctPassword) && correctPassword == model.Password)
+            {
+                HttpContext.Session.SetString("User", model.Username);
+                return RedirectToAction("Index", "Home");
+            }
+            
+            ModelState.AddModelError("", "Forkert brugernavn eller adgangskode");
+            return View(model);
         }
 
         [HttpPost]
@@ -28,9 +45,20 @@ namespace MySocialNetwork.Controllers
         }
 
         [HttpGet]
-        public IActionResult Register()
+        public IActionResult Register(UserViewModel model)
         {
-            return View();
+            if (!ModelState.IsValid)
+            return View(model);
+
+            if (users.ContainsKey(model.Username))
+            {
+                ModelState.AddModelError("Username", "Brugernavn findes allerede");
+                return View(model);
+            }
+            
+            users[model.Username] = model.Password;
+            HttpContext.Session.SetString("User", model.Username);
+            return RedirectToAction("Index", "Home");
         }
 
         [HttpPost]
---
+++ MySocialNetwork/Models/UserViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace MySocialNetwork.Models;
+
+public class UserViewModel
+{
+    [Required(ErrorMessage = "Brugernavn er påkrævet")]
+    [MinLength(3, ErrorMessage = "Brugernavn skal være mindst 3 tegn")]
+    public string Username { get; set; }
+    
+    [Required(ErrorMessage = "Adgangskode er påkærvet")]
+    [MinLength(5, ErrorMessage = "Adgangskode skal mindst 5 tegn")]
+    public string Password { get; set; } 
+}
