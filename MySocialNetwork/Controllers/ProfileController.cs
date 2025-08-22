b5429085c116337c7b4d2abe4606a9e259f3aa45
Alexander <Alexander.Friedrichsen7@gmail.com>
2025-08-22 14:10:43 +0200

    Added profil site for user.

---
+++ MySocialNetwork/Controllers/ProfileController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MySocialNetwork.Data;
+using System.Linq;
+
+
+namespace MySocialNetwork.Controllers;
+
+public class ProfileController : Controller
+{
+    private readonly AppDbContext _context;
+
+    public ProfileController(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public IActionResult Index(string username)
+    {
+        if (string.IsNullOrEmpty(username))
+        {
+            username = HttpContext.Session.GetString("User");
+        }
+
+        var posts = _context.Posts
+            .Where(p => p.Username == username)
+            .OrderByDescending(p => p.CreatedAt)
+            .ToList();
+        
+        ViewBag.Username = username;
+        return View(posts);
+    }
+}
